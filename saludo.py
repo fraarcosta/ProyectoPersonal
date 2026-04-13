6279eabfe509e34a4d5265cd402cc2ab8e2267bd
Arias Costa <f.arias.costa@accenture.com>
2026-04-13 20:21:32 +0200

    remove leaked api key

--- saludo.py
+++
@@ -1,6 +0,0 @@
-nombre = input("¿Cuál es tu nombre? ")
-print(f"¡Hola, {nombre}! Bienvenido.")
-
-edad = int(input("¿Cuántos años tienes? "))
-años_restantes = 100 - edad
-print(f"Tienes {edad} años. Te faltan {años_restantes} años para llegar a los 100.")
